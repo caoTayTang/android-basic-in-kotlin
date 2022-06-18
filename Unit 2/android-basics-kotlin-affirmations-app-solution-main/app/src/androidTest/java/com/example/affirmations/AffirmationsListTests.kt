62b4c06e13834e67eaacdd43f106bfbf2a9afc4c
lechi <lechidaitp@gmail.com>
2022-06-18 23:58:23 +0700

    Done pathway 3 (affirmations), unit 2

---
+++ Unit 2/android-basics-kotlin-affirmations-app-solution-main/app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
@@ -0,0 +1,40 @@
+package com.example.affirmations
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+import androidx.test.espresso.assertion.ViewAssertions.matches
+
+
+@RunWith(AndroidJUnit4::class)
+class AffirmationsListTests {
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun scroll_to_item() {
+
+        onView(withId(R.id.recycler_view)).perform(
+//            in case dont know the exact place to scroll - use this insted
+             // Unfortunate that it DONT WORK FOR ME
+//            RecyclerViewActions
+//                .scrollTo<RecyclerView.ViewHolder>(
+//                    withText(R.string.affirmation10)
+//                )
+
+            //so I use this instead
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9)
+        )
+
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed())
+            )
+    }
+}
